94d697857c7ae39551230c2c5cd260624fcd3879
99003741 <78871103+99003741@users.noreply.github.com>
2021-02-23 09:38:51 +0530

    added the basic details in main file

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Description: This program takes a user input as a keyword and search the occurrence of the word in the file and the
+                count total occurrences and store these details in a new file
+Author: RAJAT MEHTA
+PS No: 99003741
+Contact: rajat.mehta@ltts.com
+Date of Creation: 23/02/2021
+"""
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def search_for_word(key_word):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {key_word}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    search_word = input("Enter the word to be searched in the file")
+    search_for_word(search_word)
